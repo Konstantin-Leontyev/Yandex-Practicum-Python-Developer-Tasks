8cdecd2753a1560497e139ce8ea4925b99421ac3
Konstantin-Leontyev <K.A.Leontyev@gmail.com>
2023-12-05 18:10:00 +0400

    Add Loops and branches folder tasks

---
+++ 2. Loops and branches/lesson010/task2.py
@@ -0,0 +1,13 @@
+# Добавьте новые условия в elif и else
+for messages_count in range(0, 21):
+    if messages_count == 0:
+        print('У вас нет новых сообщений')
+    elif messages_count == 1:
+        # напишите ваш код здесь
+        print('У вас 1 новое сообщение')
+    elif messages_count > 1 and messages_count < 5:
+        # напишите ваш код здесь
+        print(f'У вас {messages_count} новых сообщения')
+    else:
+        # напишите ваш код здесь
+        print(f'У вас {messages_count} новых сообщений')
---
+++ 2. Loops and branches/lesson6/task1.py
@@ -0,0 +1,4 @@
+for messages_count in range(6):
+    # Напишите код тут
+    if messages_count > 0:
+        print('Новых сообщений:', messages_count)
